5665bf9fdaec954077ac6f4f8c695e1ddeb84fc9
Pawer Trem <109388706+pawertrem@users.noreply.github.com>
2024-07-06 15:38:54 +0300

    Update augmenter.py

--- augmenter.py
+++ augmenter.py
@@ -1,9 +1,10 @@
 from ruwordnet import RuWordNet
+from numpy import random 
+
 wn = RuWordNet(filename_or_session='ruwordnet-2021.db')
 
 # function for replacement of a word with synonym
 def get_synonym(word):
-  if word != 'бар':
     try:
       synsets = wn.get_senses(word)[0].synset.hypernyms
       synonyms = set()
@@ -12,8 +13,6 @@ def get_synonym(word):
       return random.choice(list(synonyms)) if synonyms else word
     except:
       return word
-  else:
-    return word
 
 # function for augmentation
 def augment_text(text, replace_prob = 0.8):
